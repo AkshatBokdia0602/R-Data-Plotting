1689ba00555452d73b2ccdd7d5a012ff45286ee6
Akshat Bokdia <101403537+AkshatBokdia0602@users.noreply.github.com>
2022-04-29 17:49:26 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,9 +1,27 @@
+# Program 2 to plot a time series / line plot for Global Active Power
+# using the base plotting system (graphics Package) and push the graph to a png file.
+
+# Creating a Specific Directory for this Assignment
+if (!file.exists("R_Data_Plotting"))
+  dir.create("R_Data_Plotting")
+
+# Moving the Zip file to the above Directory and unzipping it
+file.copy("~/Downloads/exdata_data_household_power_consumption.zip", "~/R_Data_Plotting")
+setwd("~/R_Data_Plotting")
+unzip("exdata_data_household_power_consumption.zip")
+
+# Reading the input data and subsetting the required data (data between the 2 days - 01/02/2007 and 02/02/2007)
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 data2 <- subset(data, data$Date == "1/2/2007"|data$Date == "2/2/2007")
-data2$Datetime <-paste(data2$Date, data2$Time)
-data2$Date <- as.Date(data2$Date, "%d/%m/%Y")
-data2$Time <- strptime(data2$Time, "%T")
 data2$Global_active_power <- with(data2, as.numeric(Global_active_power))
+
+# Creating a new column for data2 by concatenating Date and Time columns
+data2$Datetime <- strptime(paste(data2$Date, data2$Time), "%d/%m/%Y %H:%M:%S") 
+
+# Calling png() function to create a graphics device for PNG format bitmap files
 png("plot2.png", 480, 480)
-plot(strptime(data2$Datetime, "%d/%m/%Y %H:%M:%S"), data2$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power(Kilowatts)")
+
+# Calling plot() function to create a line plot for Global Active Power against the Time peroid
+plot(data2$Datetime, data2$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power(Kilowatts)")
+
 dev.off()
