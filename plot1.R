e2a38cf27193b2d4b4c9d37a0eee40609715cdfa
Akshat Bokdia <101403537+AkshatBokdia0602@users.noreply.github.com>
2022-04-29 17:33:52 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,24 @@
+# Program 1 to plot a histogram for Global Active Power
+# using the base plotting system (graphics Package) and push the graph to a png file.
+
+# Creating a Specific Directory for this Assignment
+if (!file.exists("R_Data_Plotting"))
+  dir.create("R_Data_Plotting")
+
+# Moving the Zip file to the above Directory and unzipping it
+file.copy("~/Downloads/exdata_data_household_power_consumption.zip", "~/R_Data_Plotting")
+setwd("~/R_Data_Plotting")
+unzip("exdata_data_household_power_consumption.zip")
+
+# Reading the input data and subsetting the required data (data between the 2 days - 01/02/2007 and 02/02/2007)
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 data2 <- subset(data, data$Date == "1/2/2007"|data$Date == "2/2/2007")
 data2$Global_active_power <- with(data2, as.numeric(Global_active_power))
+
+# Calling png() function to create a graphics device for PNG format bitmap files
 png("plot1.png", 480, 480)
+
+# Calling hist() function to create a histogram for Global Active Power
 hist(data2$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylim = c(0, 1200))
+
 dev.off()
